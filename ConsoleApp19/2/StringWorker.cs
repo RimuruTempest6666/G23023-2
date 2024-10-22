1c6381ce43937c49ef1c567dd721e9ca5f345016
RimuruTempest6666 <129671984+RimuruTempest6666@users.noreply.github.com>
2024-10-22 18:47:06 +0300

    Add files via upload

---
+++ ConsoleApp19/2/StringWorker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19._2
+{
+    public class StringWorker
+    {
+        public static void CountLetters(string input)
+        {
+            Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+            foreach(char c in input)
+            {
+                if (letterCount.ContainsKey(c))
+                {
+                    letterCount[c]++;
+                }
+                else
+                {
+                    letterCount[c] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in letterCount)
+            {
+                Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp19._1;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Kata.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+
+        }
+    }
+}
