96704cf545f32ab3dfde0eb77d4045e4008d0073
RimuruTempest6666 <129671984+RimuruTempest6666@users.noreply.github.com>
2024-10-24 18:47:14 +0300

    Add files via upload

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+        }
+    }
+}
